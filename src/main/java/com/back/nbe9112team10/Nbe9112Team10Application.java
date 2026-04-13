0d069596e6c758908a2bc6b5dfd21aa96cbe5062
wuho98 <dngh112@gmail.com>
2026-04-13 10:40:12 +0900

    feat: 백엔드 프로젝트 구조 정리 및 초기화

---
+++ src/main/java/com/back/nbe9112team10/Nbe9112Team10Application.java
@@ -0,0 +1,13 @@
+package com.back.nbe9112team10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe9112Team10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe9112Team10Application.class, args);
+    }
+
+}
