9cd53299fe364fbe32e594d8b55ba2abc3053a29
moon-three <moondal366@gmail.com>
2026-04-14 20:34:50 +0900

    feat: 회원가입 시 이메일,닉네임 중복 검증

--- src/main/java/com/team10/backend/domain/user/controller/AuthController.java
+++ src/main/java/com/team10/backend/domain/user/controller/AuthController.java
@@ -2,12 +2,16 @@ package com.team10.backend.domain.user.controller;
 
 import com.team10.backend.domain.user.dto.AuthRegisterRequest;
 import com.team10.backend.domain.user.dto.AuthRegisterResponse;
+import com.team10.backend.domain.user.dto.DuplicateCheckResponse;
+import com.team10.backend.domain.user.enums.DuplicateType;
 import com.team10.backend.domain.user.service.AuthService;
 import com.team10.backend.global.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +27,13 @@ public class AuthController {
         return ApiResponse.ok(response);
     }
 
+    @GetMapping("/check-duplicate")
+    public ApiResponse<DuplicateCheckResponse> checkEmail(
+                                        @RequestParam DuplicateType type,
+                                        @RequestParam String value) {
+        DuplicateCheckResponse response = authService.checkDuplicate(type, value);
+        return ApiResponse.ok(response);
+    }
 
 
 }
