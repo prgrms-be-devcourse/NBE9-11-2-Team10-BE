a96a8e735f33db6248102205ce9ab35655e51eb9
moon-three <moondal366@gmail.com>
2026-04-15 12:24:09 +0900

    feat: Swagger 내용 추가

--- src/main/java/com/team10/backend/domain/user/enums/Role.java
+++ src/main/java/com/team10/backend/domain/user/enums/Role.java
@@ -1,15 +1,6 @@
 package com.team10.backend.domain.user.enums;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum Role {
-    BUYER("ROLE_USER", "일반 사용자"),
-    SELLER("ROLE_SELLER", "판매자")
-    ;
-
-    private final String key;
-    private final String title;
+    BUYER,
+    SELLER
 }
--- src/main/java/com/team10/backend/domain/user/enums/UserStatus.java
+++ src/main/java/com/team10/backend/domain/user/enums/UserStatus.java
@@ -1,15 +1,6 @@
 package com.team10.backend.domain.user.enums;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum UserStatus {
-    ACTIVE("활성", "정상적으로 서비스를 이용 중인 상태"),
-    DELETED("탈퇴", "사용자가 자진 탈퇴하여 계정이 비활성화된 상태")
-    ;
-
-    private final String title;
-    private final String description;
+    ACTIVE,
+    DELETED
 }
