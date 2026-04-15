81b05a0403e6678e49133cd94598dbd95add651a
moon-three <moondal366@gmail.com>
2026-04-15 11:34:21 +0900

    test: 회원가입, 중복확인 테스트코드 추가 - 추가 예외처리 필요

--- src/main/java/com/team10/backend/domain/user/dto/AuthRegisterRequest.java
+++ src/main/java/com/team10/backend/domain/user/dto/AuthRegisterRequest.java
@@ -3,7 +3,6 @@ package com.team10.backend.domain.user.dto;
 import com.team10.backend.domain.user.enums.Role;
 
 public record AuthRegisterRequest(
-        String imageUrl,
         String email,
         String password,
         String name,
--- src/main/java/com/team10/backend/domain/user/dto/DuplicateCheckResponse.java
+++ src/main/java/com/team10/backend/domain/user/dto/DuplicateCheckResponse.java
@@ -1,12 +1,9 @@
 package com.team10.backend.domain.user.dto;
 
 import com.team10.backend.domain.user.enums.DuplicateType;
-import jakarta.validation.constraints.NotBlank;
 
 public record DuplicateCheckResponse(
-        @NotBlank
         DuplicateType type,
-        @NotBlank
         String value,
         boolean available
 ) {}
