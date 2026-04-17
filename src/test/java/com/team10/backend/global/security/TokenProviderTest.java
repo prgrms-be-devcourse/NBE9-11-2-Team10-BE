e57d50497d7731e3175ce63245fa50a941048434
moon-three <moondal366@gmail.com>
2026-04-17 12:13:27 +0900

    test: TokenProvider 테스트 코드 작성

--- src/test/java/com/team10/backend/global/security/TokenProviderTest.java
+++ src/test/java/com/team10/backend/global/security/TokenProviderTest.java
@@ -8,12 +8,16 @@ import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.core.Authentication;
 
 import javax.crypto.SecretKey;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TokenProviderTest {
 
@@ -36,7 +40,7 @@ public class TokenProviderTest {
     void generateToken_success() {
         // given
         Long userId = 1L;
-        Role role = Role.ROLE_BUYER;
+        Role role = Role.BUYER;
 
         // when
         String token = tokenProvider.generateToken(userId, role);
@@ -50,7 +54,7 @@ public class TokenProviderTest {
     @DisplayName("토큰에 id와 role이 포함된다")
     void token_contains_claims() {
         // given
-        String token = tokenProvider.generateToken(1L, Role.ROLE_BUYER);
+        String token = tokenProvider.generateToken(1L, Role.BUYER);
 
         // when
         Claims claims = Jwts.parser()
@@ -62,6 +66,64 @@ public class TokenProviderTest {
         // then
         assertEquals("1", claims.getSubject());
         assertEquals("BUYER", claims.get("role").toString());
+    }
+
+    @Test
+    @DisplayName("토큰 파싱이 정상적으로 된다")
+    void parseClaims_success() {
+        // given
+        String token = tokenProvider.generateToken(1L, Role.BUYER);
+
+        // when
+        Claims claims = tokenProvider.parseClaims(token);
+
+        // then
+        assertEquals("1", claims.getSubject());
+        assertEquals("BUYER", claims.get("role"));
+    }
+
+    @Test
+    @DisplayName("Authentication 객체가 정상 생성되고 권한 정보가 제대로 들어간다")
+    void getAuthentication_success() {
+        // given
+        String token = tokenProvider.generateToken(1L, Role.BUYER);
+
+        // when
+        Authentication authentication = tokenProvider.getAuthentication(token);
+
+        // then
+        assertNotNull(authentication);
+        assertEquals("1", authentication.getName());
+        assertEquals(1, authentication.getAuthorities().size());
+        assertTrue(
+                authentication.getAuthorities().stream()
+                        .anyMatch(a ->
+                                Objects.equals(a.getAuthority(), "ROLE_BUYER")));
+    }
+
+//    @Test
+//    @DisplayName("시간이 만료된 토큰은 예외가 발생한다")
+//    void parseClaims_fail_expireToken() throws InterruptedException {
+//        String token = tokenProvider.generateToken(1L, Role.BUYER);
+//
+//        Thread.sleep(60001);
+//
+//        // when & then
+//        assertThrows(ExpiredJwtException.class, () -> {
+//            tokenProvider.parseClaims(token);
+//        });
+//    }
+
+    @Test
+    @DisplayName("잘못된 토큰은 예외가 발생한다")
+    void parseClaims_fail() {
+        // given
+        String invalidToken = "fake.token.value";
+
+        // when & then
+        assertThrows(Exception.class, () -> {
+            tokenProvider.parseClaims(invalidToken);
+        });
     }
 
 }
